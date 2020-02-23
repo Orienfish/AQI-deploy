3162a52c193a2fbee1020c1920bcbe43032fa896
Orienfish <x1yu@ucsd.edu>
2020-02-22 20:14:00 -0800

    complete cov matrix calculation

---
+++ cov_matrix.m
@@ -0,0 +1,39 @@
+function [cov_mat, corr_mat] = cov_matrix(label, max_cnt)
+% Calculate the covariance matrix and correlation coefficient matrix.
+% Repeated reading is performed. Might be able to improve.
+% Question: are those zeros valid data or not?
+    cov_mat = NaN;
+    f_list = dir('./data/*Primary*.csv');
+    mat = NaN(max_cnt, length(f_list));
+    for f_idx = 1:length(f_list)
+        % obtain the list of data files
+        f_name = f_list(f_idx).name;
+        % fprintf('Processing file %s...\n', f_name);
+        % read data
+        f_path = append(f_list(f_idx).folder, '/', f_name);
+        T = readtable(f_path, 'Delimiter', ',', 'HeaderLines', 0);
+        
+        switch label
+            case 'temp'
+                valid_idx = ~isnan(T.Temperature_F);
+                mat(1:nnz(valid_idx), f_idx) = T.Temperature_F(valid_idx);
+            case 'humid'
+                valid_idx = ~isnan(T.Humidity__);
+                mat(1:nnz(valid_idx), f_idx) = T.Humidity__(valid_idx);
+            case 'pm1'
+                valid_idx = ~isnan(T.PM1_0_CF1_ug_m3);
+                mat(1:nnz(valid_idx), f_idx) = T.PM1_0_CF1_ug_m3(valid_idx);
+            case 'pm2_5'
+                valid_idx = ~isnan(T.PM2_5_CF1_ug_m3);
+                mat(1:nnz(valid_idx), f_idx) = T.PM2_5_CF1_ug_m3(valid_idx);
+            case 'pm10'
+                valid_idx = ~isnan(T.PM10_0_CF1_ug_m3);
+                mat(1:nnz(valid_idx), f_idx) = T.PM10_0_CF1_ug_m3(valid_idx);
+            otherwise
+                fprintf('Invalid label!');
+                return;
+        end
+    end
+    cov_mat = cov(mat, 'partialrows');
+    corr_mat = corrcoef(mat, 'Rows', 'pairwise');
+end
--- main.m
+++ main.m
@@ -1,11 +1,17 @@
 % main.m
+clear;
 warning('off','all')
 
+tic
 data_list = preprocess();
-lat = horzcat(data_list(:).lat);
-lon = horzcat(data_list(:).lon);
-sizedata = horzcat(data_list(:).pm2_5_avg);
-plot(lat, lon, sizedata);
+[cov_mat_pm1, corr_mat_pm1] = cov_matrix('pm1', max(horzcat(data_list(:).pm1_cnt)));
+[cov_mat_pm2_5, corr_mat_pm2_5] = cov_matrix('pm2_5', max(horzcat(data_list(:).pm2_5_cnt)));
+[cov_mat_pm10, corr_mat_pm10] = cov_matrix('pm10', max(horzcat(data_list(:).pm10_cnt)));
+toc
+%lat = horzcat(data_list(:).lat);
+%lon = horzcat(data_list(:).lon);
+%sizedata = horzcat(data_list(:).pm2_5_avg);
+%plot(lat, lon, sizedata);
 
 function plot(lat, lon, sizedata)
     % plot the locations
