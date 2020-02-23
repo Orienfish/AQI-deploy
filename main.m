f9a55f8700cce2c01d9380fac519f881d996ece8
Orienfish <x1yu@ucsd.edu>
2020-02-22 16:23:17 -0800

    plot pm2.5 avg

--- main.m
+++ main.m
@@ -1,12 +1,15 @@
 % main.m
 warning('off','all')
-data_list = preprocess();
 
-function plot(lat, lon)
+data_list = preprocess();
+lat = horzcat(data_list(:).lat);
+lon = horzcat(data_list(:).lon);
+sizedata = horzcat(data_list(:).pm2_5_avg);
+plot(lat, lon, sizedata);
+
+function plot(lat, lon, sizedata)
     % plot the locations
-    %lat = pos_list(:, 1);
-    %lon = pos_list(:, 2);
-    figure('Position', [0 0 1000 8000]);
-    gb = geobubble(lat, lon);
+    figure('Position', [0 0 1000 800]);
+    gb = geobubble(lat, lon, sizedata);
     geobasemap streets-light; % set base map style
 end
