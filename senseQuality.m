c90f48962777f80c323e75ffa3ff50a3a891b172
Orienfish <x1yu@ucsd.edu>
2020-03-19 14:07:08 -0700

    finish sensing quality function

--- senseQuality.m
+++ senseQuality.m
@@ -10,9 +10,10 @@ function [senseQual] = senseQuality(Xv, Xa, K)
 %   senseQual: the predicted sensing quality at Xv given Xa
 
 % calculate the covariance matrix at Xv given Xa
-cov_xa = gp_predict(Xv, Xa, K);
+cov_va = gp_predict(Xv, Xa, K);
+n = size(cov_va, 1);
 
 % calculate the sensing quality or the conditional entropy
-senseQual = 0.5 * log(2 * pi * exp(1) * det(cov_xa));
+senseQual = 0.5 * log(det(cov_va)) + 0.5 * n * log(2 * pi * exp(1));
 end
 
