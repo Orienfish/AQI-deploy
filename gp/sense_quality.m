178983645bee2c907abd81f6c52efc86577e755f
Orienfish <x1yu@ucsd.edu>
2020-04-04 13:12:29 -0700

    add little modification to sensing quality

--- gp/sense_quality.m
+++ gp/sense_quality.m
@@ -12,8 +12,12 @@ function [senseQuality] = sense_quality(Xv, cov_vd, Xa, cov_ad, K)
 % Return:
 %   senseQuality: the predicted sensing quality at Xv given Xa
 
+% processing in the original script
+cov_vd = cov_vd + (1e-10) * (eye(size(cov_vd, 1)));
+cov_ad = cov_ad + (1e-10) * (eye(size(cov_ad, 1)));
+
 % calculate the entropy
-H_Xv = 0.5 * log(det(cov_vd)); % the same part: 0.5 * n * log(2 * pi * exp(1));
+H_Xv = 1/2 * log2(det(cov_vd)) + 1/2 * log2((2 * pi * exp(1))^size(cov_vd,1));
 
 % calculate the conditional entropy
 % calculate the covariance matrix at Xv given Xa
@@ -22,7 +26,7 @@ Sigma_AV = Sigma_VA';
 cov_ad_inv = inv(cov_ad);
 cov_va = cov_vd - Sigma_VA * cov_ad_inv * Sigma_AV;
 % n = size(cov_va, 1);
-H_XvXa = 0.5 * log(det(cov_va)); % the same part: 0.5 * n * log(2 * pi * exp(1));
+H_XvXa = 1/2 * log2(det(cov_va)) + 1/2 * log2((2 * pi * exp(1))^size(cov_va,1));
 %fprintf('H_Xv: %f, H_XvXa: %f\n', H_Xv, H_XvXa);
 
 % calculate the sensing quality
