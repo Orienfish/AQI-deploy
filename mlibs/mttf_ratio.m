d4dcfdb79311d9d6d25d5bdfcaf99b342f3239b2
Orienfish <x1yu@ucsd.edu>
2020-06-05 16:20:30 -0700

    update mttf ratio

--- mlibs/mttf_ratio.m
+++ mlibs/mttf_ratio.m
@@ -11,8 +11,27 @@ function MTTFr = mttf_ratio(Tc)
 c = 10 * 1.1949e3 / (6.022 * 1.38);  % Ea/k
 
 % referece temperature (25 Celsius) in Kelvin
-Tref = 25 + 273.15;
+%Tref = 25;          % Celsius;
+%Iref = 50;          % mA
+%Pref = 3.3 * Iref / 1000; % reference power in W
+%Tc_old = Tref;
+%eps = 1e-1;       % error bound in the iteration
+%iter = 0;
+%while 1
+%    Tc_new = amb2core(Tref, Pref, Tc_old);
+%    if Tc_new - Tc_old < eps
+%        break;
+%    end
+%    Tc_old = Tc_new;
+%    iter = iter + 1;
+%    fprintf('it: %d Tc_new: %f Tc_old: %f, curPwr: %f\n', iter, ...
+%       Tc_new, Tc_old, Pref);
+%end
+%Tcref = Tc_new;
+%fprintf('Tcref in Celsius: %f\n', Tcref);
+Tcref = 33.010122 + 273.15; % core temperature reference in Kelvin obtained from 
+                     % the code above
 
-MTTFr = exp(c ./ Tc) / exp(c / Tref);
+MTTFr = exp(c ./ Tc) / exp(c / Tcref);
 %fprintf("%f\n", MTTFr);
 end
